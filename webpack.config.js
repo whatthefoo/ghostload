8bfae8d452b69e7b9a4d76b28cd8b94531d57588
Bård N Hovde <bnhovde@gmail.com>
2018-12-21 13:35:09 +0100

    Updated npm settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
-var path = require('path');
+const pkg = require('./package.json');
+const path = require('path');
+
+const libraryName = pkg.name;
+
 module.exports = {
-  entry: './src/index.js',
+  entry: path.join(__dirname, './src/index.js'),
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.join(__dirname, './dist'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    library: libraryName,
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true,
   },
   module: {
     rules: [
@@ -26,7 +33,25 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
+  },
   externals: {
-    react: 'commonjs react',
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM',
+    },
   },
 };
